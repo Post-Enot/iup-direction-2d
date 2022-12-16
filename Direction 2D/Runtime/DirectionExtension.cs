0daaf3d1da316cef591de4325346f2bc750c2934
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2022-12-16 16:26:27 +0300

    Рефакторинг класса методов расширения Direction
     * Все наборы флагов типа Direction.Up | Direction.Left заменены на раннее созданные флаги Direction.UpLeft.

--- Direction 2D/Runtime/DirectionExtension.cs
+++ Direction 2D/Runtime/DirectionExtension.cs
@@ -17,16 +17,16 @@ namespace IUP.Toolkits.Direction2D
         {
             return direction switch
             {
-                Direction.None                      => Vector2.zero,
-                Direction.Up                        => Vector2.up,
-                Direction.Down                      => Vector2.down,
-                Direction.Left                      => Vector2.left,
-                Direction.Right                     => Vector2.right,
-                Direction.Up | Direction.Left       => new Vector2(-1, 1),
-                Direction.Up | Direction.Right      => new Vector2(1, 1),
-                Direction.Down | Direction.Left     => new Vector2(-1, -1),
-                Direction.Down | Direction.Right    => new Vector2(1, -1),
-                _                                   => throw new DirectionValueMeaninglessException()
+                Direction.None       => Vector2.zero,
+                Direction.Up         => Vector2.up,
+                Direction.Down       => Vector2.down,
+                Direction.Left       => Vector2.left,
+                Direction.Right      => Vector2.right,
+                Direction.UpLeft     => new Vector2(-1, 1),
+                Direction.UpRight    => new Vector2(1, 1),
+                Direction.DownLeft   => new Vector2(-1, -1),
+                Direction.DownRight  => new Vector2(1, -1),
+                _                    => throw new DirectionValueMeaninglessException()
             };
         }
 
@@ -39,16 +39,16 @@ namespace IUP.Toolkits.Direction2D
         {
             return direction switch
             {
-                Direction.None                      => Vector2Int.zero,
-                Direction.Up                        => Vector2Int.up,
-                Direction.Down                      => Vector2Int.down,
-                Direction.Left                      => Vector2Int.left,
-                Direction.Right                     => Vector2Int.right,
-                Direction.Up | Direction.Left       => new Vector2Int(-1, 1),
-                Direction.Up | Direction.Right      => new Vector2Int(1, 1),
-                Direction.Down | Direction.Left     => new Vector2Int(-1, -1),
-                Direction.Down | Direction.Right    => new Vector2Int(1, -1),
-                _                                   => throw new DirectionValueMeaninglessException()
+                Direction.None       => Vector2Int.zero,
+                Direction.Up         => Vector2Int.up,
+                Direction.Down       => Vector2Int.down,
+                Direction.Left       => Vector2Int.left,
+                Direction.Right      => Vector2Int.right,
+                Direction.UpLeft     => new Vector2Int(-1, 1),
+                Direction.UpRight    => new Vector2Int(1, 1),
+                Direction.DownLeft   => new Vector2Int(-1, -1),
+                Direction.DownRight  => new Vector2Int(1, -1),
+                _                    => throw new DirectionValueMeaninglessException()
             };
         }
 
@@ -61,16 +61,16 @@ namespace IUP.Toolkits.Direction2D
         {
             return direction switch
             {
-                Direction.None                      => Vector2.zero,
-                Direction.Up                        => Vector2.up,
-                Direction.Down                      => Vector2.down,
-                Direction.Left                      => Vector2.left,
-                Direction.Right                     => Vector2.right,
-                Direction.Up | Direction.Left       => new Vector2(-1, 1),
-                Direction.Up | Direction.Right      => new Vector2(1, 1),
-                Direction.Down | Direction.Left     => new Vector2(-1, -1),
-                Direction.Down | Direction.Right    => new Vector2(1, -1),
-                _                                   => throw new DirectionValueMeaninglessException()
+                Direction.None       => Vector2.zero,
+                Direction.Up         => Vector2.up,
+                Direction.Down       => Vector2.down,
+                Direction.Left       => Vector2.left,
+                Direction.Right      => Vector2.right,
+                Direction.UpLeft     => new Vector2(-1, 1),
+                Direction.UpRight    => new Vector2(1, 1),
+                Direction.DownLeft   => new Vector2(-1, -1),
+                Direction.DownRight  => new Vector2(1, -1),
+                _                    => throw new DirectionValueMeaninglessException()
             };
         }
 
@@ -83,16 +83,16 @@ namespace IUP.Toolkits.Direction2D
         {
             return direction switch
             {
-                Direction.None                      => Vector3Int.zero,
-                Direction.Up                        => Vector3Int.up,
-                Direction.Down                      => Vector3Int.down,
-                Direction.Left                      => Vector3Int.left,
-                Direction.Right                     => Vector3Int.right,
-                Direction.Up | Direction.Left       => new Vector3Int(-1, 1),
-                Direction.Up | Direction.Right      => new Vector3Int(1, 1),
-                Direction.Down | Direction.Left     => new Vector3Int(-1, -1),
-                Direction.Down | Direction.Right    => new Vector3Int(1, -1),
-                _                                   => throw new DirectionValueMeaninglessException()
+                Direction.None       => Vector3Int.zero,
+                Direction.Up         => Vector3Int.up,
+                Direction.Down       => Vector3Int.down,
+                Direction.Left       => Vector3Int.left,
+                Direction.Right      => Vector3Int.right,
+                Direction.UpLeft     => new Vector3Int(-1, 1),
+                Direction.UpRight    => new Vector3Int(1, 1),
+                Direction.DownLeft   => new Vector3Int(-1, -1),
+                Direction.DownRight  => new Vector3Int(1, -1),
+                _                    => throw new DirectionValueMeaninglessException()
             };
         }
 
@@ -102,15 +102,15 @@ namespace IUP.Toolkits.Direction2D
         /// <returns>Возвращает true, если значение имеет смысл; иначе false.</returns>
         public static bool IsValueMakeSence(this Direction direction)
         {
-            return  direction == Direction.None ||
-                    direction == Direction.Up ||
-                    direction == Direction.Down ||
-                    direction == Direction.Left ||
-                    direction == Direction.Right ||
-                    direction == (Direction.Up | Direction.Left) ||
-                    direction == (Direction.Up | Direction.Right) ||
-                    direction == (Direction.Down | Direction.Left) ||
-                    direction == (Direction.Down | Direction.Right);
+            return (direction == Direction.None)       ||
+                   (direction == Direction.Up)         ||
+                   (direction == Direction.Down)       ||
+                   (direction == Direction.Left)       ||
+                   (direction == Direction.Right)      ||
+                   (direction == Direction.UpLeft)     ||
+                   (direction == Direction.UpRight)    ||
+                   (direction == Direction.DownLeft)   ||
+                   (direction == Direction.DownRight);
         }
     }
 }
