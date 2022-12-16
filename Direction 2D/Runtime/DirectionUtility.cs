1b6a6a4352d7ca258b5822a829d2b3db5a4fd58e
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2022-12-16 16:16:34 +0300

    Добавлен класс с набором утилит для работы с направлениями
     * Добавлены статические методы получения перебираемых наборов всех, прямых и диагональных направлений; каждый содержит опциональный флаг о необходимости включения в набор отсутствия направления.

---
+++ Direction 2D/Runtime/DirectionUtility.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace IUP.Toolkits.Direction2D
+{
+    /// <summary>
+    /// Класс, содержащий статические методы для упрощения работы с направлениями.
+    /// </summary>
+    public static class DirectionUtility
+    {
+        /// <summary>
+        /// Метод для перебора всех направлений.
+        /// </summary>
+        /// <param name="includeZeroDirection">Необходимо ли включать в перебор отсутствие 
+        /// направления?</param>
+        /// <returns>Возвращает перебираемый набор всех направлений.</returns>
+        public static IEnumerable<Direction> Directions(bool includeZeroDirection = false)
+        {
+            if (includeZeroDirection)
+            {
+                yield return Direction.None;
+            }
+            yield return Direction.Up;
+            yield return Direction.Down;
+            yield return Direction.Left;
+            yield return Direction.Right;
+            yield return Direction.UpLeft;
+            yield return Direction.UpRight;
+            yield return Direction.DownLeft;
+            yield return Direction.DownRight;
+        }
+
+        /// <summary>
+        /// Метод для перебора прямых направлений.
+        /// </summary>
+        /// <param name="includeZeroDirection">Необходимо ли включать в перебор отсутствие 
+        /// направления?</param>
+        /// <returns>Возвращает перебираемый набор прямых направлений.</returns>
+        public static IEnumerable<Direction> DirectDirections(bool includeZeroDirection = false)
+        {
+            if (includeZeroDirection)
+            {
+                yield return Direction.None;
+            }
+            yield return Direction.Up;
+            yield return Direction.Down;
+            yield return Direction.Left;
+            yield return Direction.Right;
+        }
+
+        /// <summary>
+        /// Метод для перебора диагональных направлений.
+        /// </summary>
+        /// <param name="includeZeroDirection">Необходимо ли включать в перебор отсутствие 
+        /// направления?</param>
+        /// <returns>Возвращает перебираемый набор диагональных направлений.</returns>
+        public static IEnumerable<Direction> DiagonalDirections(bool includeZeroDirection = false)
+        {
+            if (includeZeroDirection)
+            {
+                yield return Direction.None;
+            }
+            yield return Direction.UpLeft;
+            yield return Direction.UpRight;
+            yield return Direction.DownLeft;
+            yield return Direction.DownRight;
+        }
+    }
+}
