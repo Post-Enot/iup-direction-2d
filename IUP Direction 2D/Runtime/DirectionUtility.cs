f68aed8a9d9bbaed91be3c6351e4454af67e3682
Post-Enot <ve.vyshegorodsky.cold@gmail.com>
2023-03-26 16:26:32 +0300

    Добавлен метод конвертации Vector2 в Direction

--- IUP Direction 2D/Runtime/DirectionUtility.cs
+++ IUP Direction 2D/Runtime/DirectionUtility.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace IUP.Toolkits.Direction2D
 {
@@ -63,6 +65,28 @@ namespace IUP.Toolkits.Direction2D
             yield return Direction.UpRight;
             yield return Direction.DownLeft;
             yield return Direction.DownRight;
+        }
+
+        public static Direction Vector2ToDirection(Vector2 vector)
+        {
+            if (vector == Vector2.zero)
+            {
+                return Direction.None;
+            }
+            vector.Normalize();
+            float angleInDegrees = Vector2.SignedAngle(Vector2.right, vector);
+            return angleInDegrees switch
+            {
+                (>= -22.5f) and (< 22.5f) => Direction.Right,
+                (>= 22.5f) and (< 77.5f) => Direction.UpRight,
+                (>= 77.5f) and (< 112.5f) => Direction.Up,
+                (>= 112.5f) and (< 157.5f) => Direction.UpLeft,
+                (>= 157.5f) or (< -157.5f) => Direction.Left,
+                (>= -157.5f) and (< -112.5f) => Direction.DownLeft,
+                (>= -112.5f) and (< -77.5f) => Direction.Down,
+                (>= -77.5f) and (< -22.5f) => Direction.DownRight,
+                _ => throw new ArgumentOutOfRangeException(nameof(angleInDegrees))
+            }; ;
         }
     }
 }
